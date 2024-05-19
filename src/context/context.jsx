153852974fad7abce7f2ea2264042305b49faffa
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-05-19 17:08:27 +0530

    feat: Link AI with frontend for prompt processing and result display
    - Integrated the Gemini AI engine with the frontend to enable prompt
      processing and result display in the application. Implemented
      funcitonality to send user input prompts to the Gemini AI engine and
      display the corresponding results in the frontend interface.
    - Added logic to handle user input and initiate prompt processing by the
      AI engine. Implemented communication between the frontend components
      and the Gemini AI engine, ensuring seamless interaction.
    - Adjusted Main component styles to enhance visual presentation and
      ensure  a cohesive user experience.
    - Modified Main component styles to enhance visual presentation and
      ensure a cohesive user experience.
    - Fixed bugs related to input handling and result display, ensuring
      accurate processing and presentation of data.
    - This commit established the fundation for AI integration with the
      frontend, enabling dynamic interaction and response within the
      application.

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,16 +1,39 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (porps) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, SetPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
   const onSent = async (prompt) => {
-    run(prompt);
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    const response = await run(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
   };
 
-  onSent("What is react js");
-
-  const contextValue = {};
+  const contextValue = {
+    prevPrompts,
+    SetPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
 
   return (
     <Context.Provider value={contextValue}>{porps.children}</Context.Provider>
