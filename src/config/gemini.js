6d10458b932fc56876427bbd8e0ffcff21c0147b
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-05-22 17:42:47 +0530

    feat: Set default API key in gemini.js
    Set a default API key directly in the code for development purposes. This change removes the dependency on dotenv for loading environment variables and sets the API key directly. This approach simplifies development setup and ensures that the application can run smoothly without additional configuration steps.

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -13,10 +13,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-import dotenv from "dotenv";
-dotenv.config();
-
-const apiKey = process.env.GEMINI_API_KEY;
+const apiKey = "AIzaSyCrktKsyehBLgnR_8SQc945azpNssVm0YQ";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
